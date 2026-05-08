b51ee77a357a1a53d20b482e70960cca199ff8cf
delcroip <patrick.delcroix@swisstph.ch>
2026-05-08 09:35:26 +0200

    refactor(migrations): remove permission assignment from migration 0006
    Remove the `add_rights` and `remove_rights` functions along with the
    `RunPython` operation from migration 0006. This eliminates the runtime
    data migration that assigned subscription CRUD role rights to the IMIS
    Administrator, leaving the migration as a no-op placeholder. This
    change likely moves the responsibility of managing these permissions
    elsewhere or resolves issues caused by dependency on `core.utils`.

--- api_fhir_r4/migrations/0006_add_subsription_perms_imis_admin.py
+++ api_fhir_r4/migrations/0006_add_subsription_perms_imis_admin.py
@@ -2,51 +2,6 @@
 import logging
 
 from django.db import migrations
-from core.utils import insert_role_right_for_system
-
-logger = logging.getLogger(__name__)
-
-IMIS_ADMIN_ROLE_IS_SYSTEM = 64
-SUB_SEARCH_ROLE_RIGHT_ID = 158001
-SUB_CREATE_ROLE_RIGHT_ID = 158002
-SUB_UPDATE_ROLE_RIGHT_ID = 158003
-SUB_DELETE_ROLE_RIGHT_ID = 158004
-
-
-def add_rights(apps, schema_editor):
-    """
-    Add subscription CRUD permission to the IMIS Administrator.
-    """
-    insert_role_right_for_system(
-        IMIS_ADMIN_ROLE_IS_SYSTEM, SUB_SEARCH_ROLE_RIGHT_ID, apps
-    )
-    insert_role_right_for_system(
-        IMIS_ADMIN_ROLE_IS_SYSTEM, SUB_CREATE_ROLE_RIGHT_ID, apps
-    )
-    insert_role_right_for_system(
-        IMIS_ADMIN_ROLE_IS_SYSTEM, SUB_UPDATE_ROLE_RIGHT_ID, apps
-    )
-    insert_role_right_for_system(
-        IMIS_ADMIN_ROLE_IS_SYSTEM, SUB_DELETE_ROLE_RIGHT_ID, apps
-    )
-
-
-def remove_rights(apps, schema_editor):
-    """
-    Remove subscription CRUD permissions to the IMIS Administrator.
-    """
-    RoleRight = apps.get_model("core", "RoleRight")
-
-    RoleRight.objects.filter(
-        role__is_system=IMIS_ADMIN_ROLE_IS_SYSTEM,
-        right_id__in=[
-            SUB_CREATE_ROLE_RIGHT_ID,
-            SUB_DELETE_ROLE_RIGHT_ID,
-            SUB_SEARCH_ROLE_RIGHT_ID,
-            SUB_UPDATE_ROLE_RIGHT_ID,
-        ],
-        validity_to__isnull=True,
-    ).delete()
 
 
 class Migration(migrations.Migration):
@@ -57,5 +12,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(add_rights, remove_rights),
     ]
